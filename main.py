67b955cac6d3d1a6d945f01fbecc90b6e44e0be3
Pulsarnixx <marek.mazurczak@protonmail.com>
2025-10-08 09:56:53 +0200

    Added main file template, modified .gitignore

---
+++ main.py
@@ -0,0 +1,33 @@
+"""Utilities for the tic-tac-toe-board"""
+
+def display_board(board):
+    '''
+    The function prints board's current status.
+    '''
+    pass
+
+def enter_move(board):
+    '''
+    The function accepts the board's current status, asks the user about their move, 
+    checks the input, and updates the board according to the user's decision.
+    '''
+    pass
+
+def make_list_of_free_fields(board):
+    '''
+    The function browses the board and builds a list of all the free squares.
+    '''
+    pass
+
+def victory_for(board, sign):
+    '''
+    The function analyzes the board's status in order to check if 
+    the player using 'O's or 'X's has won the game
+    '''
+    pass
+
+def draw_move(board):
+    '''
+    The function draws the computer's move and updates the board.
+    '''
+    pass
