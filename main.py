9be5e067a03b789237a36f6af7e86ad26d6e8b31
Pulsarnixx <marek.mazurczak@protonmail.com>
2025-10-11 11:45:45 +0200

    Added display_board(), enter_move(), make_list_of_free_fields(), draw_move()

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
-"""Utilities for the tic-tac-toe-board"""
+import random
 
+"""Utilities for the tic-tac-toe-board"""
 def display_board(board):
     '''
     The function prints board's current status.
     '''
-    pass
+    for row in board:
+        print('+','+','+','+', sep='-------')
+        print('|','|','|','|', sep='       ')
+        print('|', row[0], '|', row[1], '|', row[2], '|', sep='   ')
+        print('|','|','|','|', sep='       ')
+
+    print('+','+','+','+', sep='-------')
 
 def enter_move(board):
     '''
     The function accepts the board's current status, asks the user about their move, 
     checks the input, and updates the board according to the user's decision.
     '''
-    pass
+    try:
+        move = int(input('Enter your move: '))
+    except:
+        print("Value type is not integer. Try again.")
+        exit()
+
+    if move not in make_list_of_free_fields(board):
+        print("This field is not free! Try again.")
+        exit()
+    
+    row = (move  - 1)// 3
+    col = (move  - 1) % 3
+
+    board[row][col] = 'O'
 
 def make_list_of_free_fields(board):
     '''
     The function browses the board and builds a list of all the free squares.
     '''
-    pass
+    return [elem for row in board for elem in row if type(elem) is int]    
 
 def victory_for(board, sign):
     '''
@@ -30,4 +50,31 @@ def draw_move(board):
     '''
     The function draws the computer's move and updates the board.
     '''
-    pass
+
+    free = make_list_of_free_fields(board)
+
+    if len(free) == 9:
+        board[1][1] = 'X'
+    else:
+        move = random.choice(free)
+        
+        row = (move  - 1)// 3
+        col = (move  - 1) % 3
+
+        board[row][col] = 'X'
+
+
+    display_board(board)
+
+board = [[3 * j + i + 1 for i in range(3)] for j in range(3)]
+
+while len(make_list_of_free_fields(board)) > 1:
+
+    draw_move(board)
+
+    enter_move(board)
+
+else:
+    draw_move(board)
+
+print("Program end.")
